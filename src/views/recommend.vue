f30396af00714a07b6a06dea45c5b1d87f984ad4
fanchaowei <597064177@qq.com>
2022-09-08 22:25:20 +0800

    完成轮播图数据获取

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,11 +1,27 @@
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
+import { getRecommend } from '@/service/recommend.js'
+
 export default defineComponent({
   name: 'recommend',
 })
 </script>
 
-<script setup></script>
+<script setup>
+// 获取歌单和轮播图数据
+async function getRecommendData() {
+  const result = await getRecommend()
+  console.log(result)
+}
+
+//#region 生命周期
+
+onMounted(async () => {
+  getRecommendData()
+})
+
+//#endregion
+</script>
 
 <template>
   <div>Recommend</div>
