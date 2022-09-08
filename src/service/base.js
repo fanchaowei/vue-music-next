a204e24efcc25d1474bfc756d797cda579be8ba9
fanchaowei <597064177@qq.com>
2022-09-08 22:24:28 +0800

    axios 封装

---
+++ src/service/base.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+// 接口响应成功的 code 值
+const ERR_OK = 0
+const baseURL = '/'
+
+axios.defaults.baseURL = baseURL
+
+// 封装 get 请求
+export function get(url, params) {
+  return axios
+    .get(url, {
+      params,
+    })
+    .then((res) => {
+      const serviceData = res.data
+      if (serviceData.code === ERR_OK) {
+        return serviceData
+      }
+    })
+    .catch((e) => {
+      console.log('axios', e)
+    })
+}
