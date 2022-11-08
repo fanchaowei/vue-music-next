5d3403d2bb2c2f96ab99f16a4b987368e2f139a8
fanchaowei <597064177@qq.com>
2022-11-08 21:28:16 +0800

    完成 4.5

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -27,8 +27,32 @@ const bgImageRef = ref(null)
 // 图片高度
 const imageHeight = ref(0)
 const bgImageStyle = computed(() => {
+  // 实现歌单列表向上滑动，能盖住歌手图片
+  const scrollYVal = scrollY.value
+  let zIndex = 0
+  let paddingTop = '70%'
+  let height = 0
+  let translateZ = 0
+  if (scrollYVal > maxTranslateY.value) {
+    zIndex = 10
+    paddingTop = 0
+    height = `${RESERVED_HEIGHT}px`
+    translateZ = 1
+  }
+
+  // 实现歌单列表向下滑动，歌手图片会放大的功能
+  let scale = 1
+  if (scrollYVal < 0) {
+    // scale 是放大或缩小像素
+    scale = 1 + Math.abs(scrollYVal / imageHeight.value)
+  }
+
   return {
+    zIndex,
+    height,
+    paddingTop,
     backgroundImage: `url(${pic.value})`,
+    transform: `scale(${scale})translateZ(${translateZ}px)`,
   }
 })
 const scrollStyle = computed(() => {
@@ -37,12 +61,26 @@ const scrollStyle = computed(() => {
   }
 })
 
+// 滚动组件当前的 y 轴值
+const scrollY = ref(0)
+// title 标题的高度
+const RESERVED_HEIGHT = 40
+// 滚动组件最大能滚动的距离
+const maxTranslateY = ref(0)
+// 实时获取滚动组件的 y 轴值
+const onScroll = (pos) => {
+  scrollY.value = -pos.y
+}
+
+// 返回
 const goBack = () => {
   router.back()
 }
-
 onMounted(() => {
+  // 获取图片的高度
   imageHeight.value = bgImageRef.value.clientHeight
+  // 获取滚动组件最大能滚动的距离
+  maxTranslateY.value = imageHeight.value - RESERVED_HEIGHT
 })
 </script>
 
@@ -55,7 +93,13 @@ onMounted(() => {
     <div class="bg-image" :style="bgImageStyle" ref="bgImageRef">
       <div class="filter"></div>
     </div>
-    <Scroll class="list" :style="scrollStyle" v-loading="loading">
+    <Scroll
+      class="list"
+      :style="scrollStyle"
+      v-loading="loading"
+      :probe-type="3"
+      @scroll="onScroll"
+    >
       <div class="song-list-wrapper">
         <SongList :songs="songs"></SongList>
       </div>
@@ -98,7 +142,6 @@ onMounted(() => {
     width: 100%;
     transform-origin: top;
     background-size: cover;
-    padding-top: 70%;
     .play-btn-wrapper {
       position: absolute;
       bottom: 20px;
